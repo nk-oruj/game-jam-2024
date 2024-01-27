2d687c13c7c60b5b863d7f4151efb9a79f1fb3af
nk-oruj <narekorujyan@gmail.com>
2024-01-27 17:16:15 +0400

    menu-game transition

--- Assets/Managers/Scripts/UIManager.cs
+++ Assets/Managers/Scripts/UIManager.cs
@@ -1,8 +1,51 @@
 using UnityEngine;
+using Cinemachine;
+using System.Collections;
 
 public class UIManager : Singleton<UIManager>
 {
-    
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+    [SerializeField] private GameObject _menuCanvas;
+    [SerializeField] private GameObject _gameCanvas;
+
+    [SerializeField] private float _menuLensSize = 8f;
+    [SerializeField] private float _gameLensSize = 5f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _menuCanvas.SetActive(true);
+        _gameCanvas.SetActive(false);
+
+        _virtualCamera.m_Lens.OrthographicSize = _menuLensSize;
+    }
+
+    private IEnumerator ZoomIn(float duration)
+    {
+        for (float t = 0f; t < 1f; t += Time.deltaTime / duration)
+        {
+            _virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(_virtualCamera.m_Lens.OrthographicSize, _gameLensSize, t);
+            yield return null;
+        }
+
+        _virtualCamera.m_Lens.OrthographicSize = _gameLensSize;
+        yield return null;
+    }
+
+    public void StartGame()
+    {
+        _menuCanvas.SetActive(false);
+        _gameCanvas.SetActive(true);
+
+        StartCoroutine(ZoomIn(.8f));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     public void SwitchCharacter(CharacterType type)
     {
         CharacterSwitchManager.Instance.SwitchCharacter(type);
