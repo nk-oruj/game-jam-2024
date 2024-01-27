9ab5a6b53efbe6742dc1835623562b7ede7a7df6
Artak <jarzop@gmail.com>
2024-01-27 19:09:46 +0400

    Added subZero pickingup mechanic

--- Assets/Scripts/Main/Characters/Bullet.cs
+++ Assets/Scripts/Main/Characters/Bullet.cs
@@ -15,8 +15,17 @@ public class Bullet : MonoBehaviour
     public void SetFlyingDirection(Vector2 direction)
     {
         _rigidbody.velocity =_speed * new Vector3(direction.x, direction.y, transform.position.z).normalized;
+        
     }
 
-    
-    
+    private void Update()
+    {
+        transform.up = _rigidbody.velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _rigidbody.velocity = Vector2.zero;
+    }
+
 }
