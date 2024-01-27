77048a8fbc9fc1d8dca644feae879e27a4cea0d2
nk-oruj <narekorujyan@gmail.com>
2024-01-27 18:54:59 +0400

    updated cage

--- Assets/Scripts/Main/Evironment/Cage.cs
+++ Assets/Scripts/Main/Evironment/Cage.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Cage : MonoBehaviour
 {
+    private Transform _transform;
+    private bool _isBroken;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _isBroken = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (_isBroken) return;
         if (other.gameObject.tag != "SubZero") return;
 
         UIManager.Instance.EnableButtonParrot();
-        Destroy(this.gameObject);
+        _transform.Rotate(new Vector3(0, 0, 45f));
+
+        _isBroken = true;
     }
 }
