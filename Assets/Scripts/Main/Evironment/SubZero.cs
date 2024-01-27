7067d5a1e597605351ed2cd2b86123560906e5b6
Artak <jarzop@gmail.com>
2024-01-28 03:33:49 +0400

    Fixed bug
    Fixed SubZero's bug

--- Assets/Scripts/Main/Evironment/SubZero.cs
+++ Assets/Scripts/Main/Evironment/SubZero.cs
@@ -5,12 +5,14 @@ public class SubZero : InteractableObject
     public override void Interact(CharacterType type)
     {
         base.Interact(type);
+        if (type == CharacterType.Mike) return;
         AllowMikeHitting();
         Destroy(transform.parent.gameObject);
     }
 
     private void AllowMikeHitting()
     {
+        
         FindObjectsOfType<Mike>()[0].GetComponent<Mike>().AllowHitting();
     }
 }
